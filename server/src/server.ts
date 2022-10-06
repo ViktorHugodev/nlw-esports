fef875ad61eab8ad2ec0d330e9d6830690455f8b
viktorhugodev <viktorhugo.dev@gmail.com>
2022-10-06 09:24:15 -0300

    create game and ads tables

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,40 @@
-import express from 'express'
-
+import express, { response } from 'express'
+import { PrismaClient } from '@prisma/client'
 const app = express()
+//initialize prisma with logs by default
+const prisma = new PrismaClient({
+  log: ['query']
+})
 
-app.get('ads')
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include:{
+      _count:{
+        select:{
+          ads: true
+        }
+      }
+    }
+  })
+  return response.status(200).json(games)
+})
+
+app.post('/ads', (request, response) => {
+  return response.status(200).json([])
+})
+
+app.get('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id
+  const ads = await prisma.ad.findMany({
+    where:{
+      gameId
+    }
+  })
+  return response.status(200).json(ads)
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+  return response.status(200).json([])
+})
 
 app.listen(3333, () => console.log('Running on port 3333'))
