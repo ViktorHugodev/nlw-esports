de2f4b0a979ebe7cf0c949f000b32fd336311074
viktorhugodev <viktorhugo.dev@gmail.com>
2022-10-15 10:18:55 -0300

    add notifications, listener

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,18 +1,30 @@
+import { useEffect, useRef } from 'react'
 import { StatusBar } from 'react-native';
 import { StyleSheet, Text } from 'react-native';
 import { Background } from './src/components/Background';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter'
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
+import { Subscription } from 'expo-modules-core'
+
+import './src/service/notificationConfigs'
+import { getPushNotificationToken } from './src/service/getPushNotificationToken'
+
+const getNotificationListener = useRef<Subscription>()
+const responseNotificationListener = useRef<Subscription>()
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black
   })
+  useEffect(() => {
+    getNotificationListener()
+  },[])
   return (
     <Background>
       <Text>Hello NLW-E Sports - Test</Text>
       <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
-      {fontsLoaded ? <Routes /> : <Loading/> }
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
